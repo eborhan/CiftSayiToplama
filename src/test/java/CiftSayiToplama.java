ad7c48307532443085c339b8dc8c6307aee475be
eborhan <106884905+eborhan@users.noreply.github.com>
2022-10-09 15:59:23 +0300

    Update CiftSayiToplama.java

--- src/test/java/CiftSayiToplama.java
+++ src/test/java/CiftSayiToplama.java
@@ -8,9 +8,9 @@ public class CiftSayiToplama {
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.print("Bir sayi giriniz: ");
+            System.out.print("Bir sayı giriniz: ");
             n = input.nextInt();
-            if (n % 2 == 0) { // Ödev için de 4'ün katı da belirtiliyor fakat mode 2 her zaman mode 4'ü de kapsar.
+            if (n % 2 == 0) { // Ödev içinde 4'ün katı da belirtiliyor fakat mode 2 her zaman mode 4'ü kapsar.
                 sum += n;
             }
         } while (n % 2 == 0);
