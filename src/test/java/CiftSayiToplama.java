ed36daaf3981596d4c7bb70854dedb7b79af94a7
eborhan <106884905+eborhan@users.noreply.github.com>
2022-10-09 15:58:05 +0300

    Update CiftSayiToplama.java

--- src/test/java/CiftSayiToplama.java
+++ src/test/java/CiftSayiToplama.java
@@ -13,7 +13,7 @@ public class CiftSayiToplama {
             if (n % 2 == 0) { // Ödev için de 4'ün katı da belirtiliyor fakat mode 2 her zaman mode 4'ü de kapsar.
                 sum += n;
             }
-        } while ((n % 2 == 0) && (n > 0));
+        } while (n % 2 == 0);
             System.out.println("Hatalı değer. Program sonlandırılıyor...");
 
         System.out.println("Toplam: " + sum);
