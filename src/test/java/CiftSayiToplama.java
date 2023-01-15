2404a5d767c3470d590661cebfd93f8b6cabb89e
eborhan <106884905+eborhan@users.noreply.github.com>
2023-01-15 15:10:40 +0300

    Final commit

--- src/test/java/CiftSayiToplama.java
+++ src/test/java/CiftSayiToplama.java
@@ -8,14 +8,14 @@ public class CiftSayiToplama {
         Scanner input = new Scanner(System.in);
 
         do {
-            System.out.print("Bir sayi giriniz: ");
+            System.out.print("Bir sayı giriniz: ");
             n = input.nextInt();
-            if (n % 2 == 0) { // Ödev için de 4'ün katı da belirtiliyor fakat mode 2 her zaman mode 4'ü de kapsar.
+            if (n % 2 == 0) { // Ödev içinde 4'ün katı da belirtiliyor fakat mode 2 her zaman mode 4'ü kapsar.
                 sum += n;
             }
-        } while ((n % 2 == 0) && (n > 0));
-            System.out.println("Hatalı değer. Program sonlandırılıyor...");
+        } while (n % 2 == 0);
+        System.out.println("Hatalı değer. Program sonlandırılıyor...");
 
-        System.out.println("Toplam: " + sum);
+        System.out.println("Girilen çift sayıların toplamı: " + sum);
     }
 }
